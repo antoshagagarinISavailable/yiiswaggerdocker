711f70e0c4dd941fabc7c26c16c07c43ad4e7cdc
Колесников Вадим <cr_ovr_web8@efko.ru>
2023-10-23 18:38:03 +0300

    openapi, swagger, frontend, minimal

--- controllers/ApiController.php
+++ controllers/ApiController.php
@@ -3,8 +3,14 @@
 namespace app\controllers;
 
 use app\models\CalculatePrice;
+use app\models\Months;
+use app\models\Tonnages;
+use app\models\RawTypes;
 use app\models\GetData;
 use yii\rest\Controller;
+use yii\web\Response;
+use Yii;
+
 
 class ApiController extends Controller
 {
@@ -29,14 +35,76 @@ class ApiController extends Controller
         $model = new CalculatePrice();
         \Yii::$app->response->format = \yii\web\Response::FORMAT_JSON;
         $request = \Yii::$app->request;
+
         if ($request->isPost && $request->getRawBody()) {
             $data = json_decode($request->getRawBody(), true);
             if (
-                (isset($data['raw']) &&
+                (isset($data['type']) &&
                     isset($data['month']) &&
                     isset($data['tonnage']))
             ) {
                 return $model->calculatePriceRes();
+            }
+        }
+        throw new \yii\web\HttpException(418, "ПЕЙ ЧАЙ");
+    }
+    public function actionGetSpec()
+    {
+        Yii::$app->response->format = Response::FORMAT_RAW;
+        Yii::$app->response->headers->set('Content-type', 'application/x-yaml');
+
+        ob_start();
+
+        include_once Yii::getAlias('@app') . '/swagger/spec.yml';
+
+        return ob_get_clean();
+    }
+
+    public function actionGetMonth()
+    {
+        Yii::$app->response->format = \yii\web\Response::FORMAT_JSON;
+        $model = new Months();
+        return $model->getListForSelect();
+    }
+    public function actionGetTypes()
+    {
+        Yii::$app->response->format = \yii\web\Response::FORMAT_JSON;
+        $model = new RawTypes();
+        return $model->getListForSelect();
+        throw new \yii\web\HttpException(418, "ПЕЙ ЧАЙ");
+    }
+    public function actionGetTonnages()
+    {
+        Yii::$app->response->format = \yii\web\Response::FORMAT_JSON;
+        $model = new Tonnages();
+        return $model->getListForSelect();
+        throw new \yii\web\HttpException(418, "ПЕЙ ЧАЙ");
+    }
+    public function actionGetCalculate()
+    {
+        $model = new CalculatePrice();
+        \Yii::$app->response->format = \yii\web\Response::FORMAT_JSON;
+        $request = \Yii::$app->request;
+        if ($request) {
+            $almost_data = $request->getQueryString();
+            $decodedData = urldecode($almost_data);
+
+            $pairs = explode('&', $decodedData);
+
+            $data = array();
+
+            foreach ($pairs as $pair) {
+                $keyValue = explode('=', $pair);
+                $key = $keyValue[0];
+                $value = $keyValue[1];
+                $data[$key] = $value;
+            }
+            if (
+                (isset($data['type']) &&
+                    isset($data['month']) &&
+                    isset($data['tonnage']))
+            ) {
+                return $model->calculatePriceRes($data);
             }
         }
         throw new \yii\web\HttpException(418, "ПЕЙ ЧАЙ");
